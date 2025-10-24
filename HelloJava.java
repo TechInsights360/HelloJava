7fa7629432cb0fa0b9d3d989d313778208c1fcec
techinsights360 <239902148+TechInsights360@users.noreply.github.com>
2025-10-23 22:57:20 -0400

    Updated message in HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,5 @@
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub!");
+        System.out.println("Hello again from TechInsights360!");
     }
 }
